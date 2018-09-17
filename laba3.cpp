b2d468908c5f29386d002e71280661a8f4d2d3bb
lesor9 <lesor@protonmail.com>
2018-09-18 01:11:56 +0300

    Added 3rd lab

--- laba3.cpp
+++ laba3.cpp
@@ -1,35 +1,38 @@
-#include <iomanip.h>
+#include <iomanip>
 #include <math.h>
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-
-	double a, b, h, x, y, s, p, p1;
+	
+	double a, b, h, x, y, s, z;
 	int n, i;
+	long double m;
 
 	n = 140;
 	x = a = 0.1;
 	b = 1;
+	m = 1;
+	h = (b - a) / 10;
 
 	do
 	{
 		s = 0;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (int j = 1; j <= i; j++)
 			{
 				m *= j;
 			}
 
-			z = ((2 * n + 1) * (pow(x, 2 * n))) / m;
+			z = ((2 * i + 1) * (pow(x, 2 * i))) / m;
+			m = 1;
 			s += z;
-
-			y = (1 + 2 * pow(x, 2)) * exp(pow(x, 2);
 		}
+		
+		y = (1 + 2 * pow(x, 2)) * exp(pow(x, 2));
 
 		cout << setw(15) << x << setw(15) << y << setw(15) << s << endl;
 		x += h;
